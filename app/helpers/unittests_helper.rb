2a175649bb34610a2079a2a3a1d8215e0f344727
Erik Kastner <kastner@gmail.com>
2009-02-13 17:18:32 -0500

    helper approach

--- app/helpers/unittests_helper.rb
+++ app/helpers/unittests_helper.rb
@@ -1,2 +1,12 @@
 module UnittestsHelper
+  def full_url(url)
+    host = url.testurl.gsub(/https?:\/\//, '')
+    attrs = url.attributes.merge(:host => host)
+    attrs.delete("testurl")
+    attrs.delete("created_at")
+    attrs.delete("updated_at")
+    attrs.delete("id")
+
+    the_url = url_for(attrs)
+  end
 end
