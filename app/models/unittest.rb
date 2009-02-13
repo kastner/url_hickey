cffa901ecff69d2cae674c4cbe25c78a3c314166
Erik Kastner <kastner@gmail.com>
2009-02-13 17:13:17 -0500

    The model way of doing the full url

--- app/models/unittest.rb
+++ app/models/unittest.rb
@@ -1,2 +1,11 @@
 class Unittest < ActiveRecord::Base
+  def full_url
+    # "#{testurl}?propurl=#{propurl}&basehref=#{basehref}&framework=#{framework}&hinav&#{hinav}&treecrumb=#{treecrumb}&titles=#{titles}&sectiontitle=#{sectiontitle}&backpack=#{backpack}&popup=#{popup}&Rrptlcount=#{rrptlcount}"
+    url_attrs = attributes.dup
+    url_attrs.delete("testurl")
+    url_attrs.delete("updated_at")
+    url_attrs.delete("created_at")
+    url_attrs.delete("id")
+    testurl + "?" + url_attrs.map{|k,v| "#{k}=#{v}"}.join("&")
+  end
 end
